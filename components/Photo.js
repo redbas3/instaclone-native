6c920e254160d4b1f5662ad4bd3ba07f780523c3
RJTmini <redbas3@gmail.com>
2023-08-02 16:09:00 +0900

    #16.7 end

--- components/Photo.js
+++ components/Photo.js
@@ -1,12 +1,24 @@
-import React from "react";
-import { useWindowDimensions } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { useWindowDimensions, Image } from "react-native";
+import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View``;
-const Header = styled.View``;
-const UserAvatar = styled.Image``;
+const Header = styled.TouchableOpacity`
+  padding: 10px;
+  flex-direction: row;
+  align-items: center;
+`;
+const UserAvatar = styled.Image`
+  margin-right: 10px;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+`;
 const Username = styled.Text`
   color: white;
+  font-weight: 600;
 `;
 const File = styled.Image``;
 const Actions = styled.View``;
@@ -20,21 +32,35 @@ const CaptionText = styled.Text`
 `;
 
 export default function Photo({ id, user, caption, file, isLiked, likes }) {
-  const { width, height } = useWindowDimensions();
+  const navigation = useNavigation();
+  const { width: screenWidth, height: screenHeight } = useWindowDimensions();
+  const [imageHeight, setImageHeight] = useState(screenHeight - 450);
+  useEffect(() => {
+    Image.getSize(file, (width, height) => {
+      setImageHeight(height / (width / screenWidth));
+    });
+  }, []);
+
   return (
     <Container>
-      <Header>
-        <UserAvatar />
+      <Header onPress={() => navigation.navigate("Profile")}>
+        <UserAvatar resizeMode="cover" source={{ uri: user.avatar }} />
         <Username>{user.username}</Username>
       </Header>
-      <File style={{ width, height: height - 500 }} source={{ uri: file }} />
+      <File
+        resizeMode="cover"
+        style={{ width: screenWidth, height: imageHeight }}
+        source={{ uri: file }}
+      />
       <Actions>
         <Action />
         <Action />
       </Actions>
       <Likes>{likes === 1 ? "1 lik" : `${likes} likes`}</Likes>
       <Caption>
-        <Username>{user.username}</Username>
+        <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
+          <Username>{user.username}</Username>
+        </TouchableOpacity>
         <CaptionText>{caption}</CaptionText>
       </Caption>
     </Container>
