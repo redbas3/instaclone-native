2ecd9e2fd77517a31a04481d5bc2f8a646a458ee
RJTmini <redbas3@gmail.com>
2023-08-04 23:48:48 +0900

    #17.3 end

--- screens/Search.js
+++ screens/Search.js
@@ -1,18 +1,43 @@
+import { useEffect } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
+import styled from "styled-components";
+import DismissKeyboard from "../components/DismissKeyboard";
+import { useForm } from "react-hook-form";
+
+const Input = styled.TextInput``;
 
 export default function Search({ navigation }) {
+  const { setValue, register } = useForm();
+  const SearchBox = () => (
+    <Input
+      style={{ backgroundColor: "white" }}
+      placeholderTextColor="black"
+      placeholder="Search photos"
+      autoCapitalize="none"
+      returnKeyLabel="Search"
+      autoCorrect={false}
+      onChangeText={(text) => setValue("keyword", text)}
+    />
+  );
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitle: SearchBox,
+    });
+    register("keyword");
+  }, []);
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "black",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <TouchableOpacity onPress={() => navigation.navigate("Photo")}>
+    <DismissKeyboard>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "black",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <Text style={{ color: "white" }}>Photo</Text>
-      </TouchableOpacity>
-    </View>
+      </View>
+    </DismissKeyboard>
   );
 }
